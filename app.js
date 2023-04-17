6865bd20885a2169dbaf179a3c83dd0739a53acd
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-17 11:52:23 -0400

    Update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Budget App! For Docs, please visit...");
+    res.send("Welcome to Budget App! For Docs, please visit ...");
 });
 
 module.exports = app;
