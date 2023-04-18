433f15cf100a99fcfcab47c15796518f7fb4386f
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-18 12:15:40 -0400

    Edit validators.js and transactionsController.js

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -4,7 +4,12 @@ const transactionsArray = require("../models/transactions");
 const { validateObject, transactionsValidator } = require("../models/validators");
 
 transactions.get("/", (req, res) => {
-    res.json(transactionsArray);
+    const { error } = req.query;
+    if (error) {
+        res.send(`<h1>${error}</h1>`)
+    } else {
+        res.json(transactionsArray);
+    }
 });
 
 transactions.post("/", validateObject, transactionsValidator, (req, res) => {
@@ -21,18 +26,24 @@ transactions.get("/:id", (req, res) => {
     if (transactionsArray[index]) {
         res.json(transactionsArray[index]);
     } else {
-        res.redirect("/transactions").status(404).json({error: "Transaction Not Found"});
+      res.status(404);
+      res.redirect("/transactions?error=Transaction%20Not%20Found");
     }
 });
 
 transactions.put("/:id", validateObject, transactionsValidator, (req, res) => {
     const { id } = req.params;
+    const transactionInfo = transactionsArray.find(
+      (transaction) => transaction.id === id
+    );
+    const index = transactionsArray.indexOf(transactionInfo);
 
-    if (transactionsArray[id]) {
-        transactionsArray[id] = req.body;
-        res.status(200).json(transactionsArray[id]);
+    if (transactionsArray[index]) {
+        transactionsArray[index] = req.body;
+        res.status(200).json(transactionsArray[index]);
     } else {
-        res.status(404).json({error: "Not Found"});
+        res.status(404);
+        res.redirect("/transactions?error=Transaction%20Not%20Found");
     }
 });
 
@@ -47,7 +58,7 @@ transactions.delete("/:id", (req, res) => {
         const deletedTransaction = transactionsArray.splice(index, 1);
         res.status(200).json(deletedTransaction);
     } else {
-        res.status(404).json({error: "Not Found"});
+        res.status(404).json({error: "Transaction Not Found"});
     }
 });
 
--- models/validators.js
+++ models/validators.js
@@ -12,10 +12,11 @@ const validateObject = [
 ];
 
 const transactionsValidator = (req, res, next) => {
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.log(errors);
-        return res.status(400).json({errors: errors.array});
+
+        return res.status(400).json({error: "error" });
     }
     next();
 };
