57de7bf4c182484604081ba3e58db82ab017e86a
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-18 14:32:21 -0400

    add to transactions model

--- models/transactions.js
+++ models/transactions.js
@@ -1,11 +1,27 @@
 
 module.exports = [
-    {
-        id: "",
-        item_name: "",
-        amount: "",
-        date: "",
-        from: "",
-        category: ""
-    }
-]
+  {
+    id: "73289",
+    item_name: "Electric",
+    amount: "100",
+    date: "April 1",
+    from: "ConEddy",
+    category: "Utilities",
+  },
+  {
+    id: "839754",
+    item_name: "Income",
+    amount: "3000",
+    date: "April 15",
+    from: "Google",
+    category: "Income",
+  },
+  {
+    id: "6387",
+    item_name: "Groceries",
+    amount: "300",
+    date: "April 12",
+    from: "Grocery store",
+    category: "Food",
+  },
+];
